33c5af8ba6f47691056d9c7ac0bee1b62a2e2df7
RadonZ <supachai27102000@gmail.com>
2024-05-10 22:16:59 +0700

    create dashboard

--- Cilents/src/feature/dashboard/DashboardLayout.tsx
+++ Cilents/src/feature/dashboard/DashboardLayout.tsx
@@ -1,8 +1,32 @@
 import styled from "styled-components";
 
+import { useRecentBooking } from "./useRecentBooking";
+import { useRecentStays } from "./useRecentStays";
+
+import Spinner from "../../ui/Spinner/Spinner";
+
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+
+
+export default function DashboardLayout() {
+  const {bookings, isLoading: isLoading1} = useRecentBooking();
+  const {stays, confirmedStays, isLoading: isLoading2} = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner/>
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  )
+}
