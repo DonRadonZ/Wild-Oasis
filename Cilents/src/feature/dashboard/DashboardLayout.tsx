8ea0d603d4be948338403e7bbf4d531db51ebd0d
RadonZ <supachai27102000@gmail.com>
2024-05-13 20:32:18 +0700

    finish chart

--- Cilents/src/feature/dashboard/DashboardLayout.tsx
+++ Cilents/src/feature/dashboard/DashboardLayout.tsx
@@ -7,6 +7,7 @@ import Spinner from "../../ui/Spinner/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 
 const StyledDashboardLayout = styled.div`
@@ -33,7 +34,7 @@ export default function DashboardLayout() {
         numDays={numDays} 
         cabinCount={cabins.length}
         />
-      <div>Today's activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
--- Cilents/src/feature/settings/UpdateSettingsForm.tsx
+++ Cilents/src/feature/settings/UpdateSettingsForm.tsx
@@ -1,6 +1,6 @@
 import Form from '../../ui/Form/Form';
 import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Input from '../../ui/Input/Input';
 
 function UpdateSettingsForm() {
   return (
