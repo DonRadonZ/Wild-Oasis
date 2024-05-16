c29ec515218480a0ed53dcbcdb88ee7eaf6a2d00
RadonZ <supachai27102000@gmail.com>
2024-05-16 13:20:44 +0700

    add setform

--- Cilents/src/feature/cabins/CabinRow.tsx
+++ Cilents/src/feature/cabins/CabinRow.tsx
@@ -3,6 +3,8 @@ import { formatCurrency } from '../../utils/helper';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCabins } from '../../services/apiCabins';
 import toast from "react-hot-toast";
+import { useState } from "react";
+import CreateCabinForm from "./CreateCabinForm";
 
 const TableRow = styled.div`
   display: grid;
@@ -46,6 +48,7 @@ const Discount = styled.div`
 
 
 export default function CabinRow({cabin}) {
+  const [showForm, setShowForm] = useState(false);
 
   const {id: cabinId ,name, maxCapacity, regularPrice, discount, image} = cabin;
 
@@ -64,13 +67,19 @@ export default function CabinRow({cabin}) {
   })
 
   return (
+    <>
     <TableRow role="row">
       <Img src={image} />
       <Cabin>{name}</Cabin>
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
+      <div>
+        <button onClick={() => setShowForm((show) => !show)}>Edit</button>
       <button onClick={() => mutate(cabinId)} disabled={isDeleting}>Delete</button>
+      </div>
     </TableRow>
+    {showForm && <CreateCabinForm cabinToEdit={cabin} />}
+    </>
   )
 }
