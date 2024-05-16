f8dee46449c3a960295482b7f23e6eda4d5a2687
RadonZ <supachai27102000@gmail.com>
2024-05-16 13:21:16 +0700

    add create cabin

--- Cilents/src/services/apiCabins.ts
+++ Cilents/src/services/apiCabins.ts
@@ -11,6 +11,41 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+    // 1. Create cabin
+    const { data, error } = await supabase
+    .from('cabins')
+    .insert([{...newCabin, image: imagePath}])
+
+    if (error) {
+      console.error(error);
+      throw new Error("Cabins could not be created");
+    }
+
+    // 2. Upload image
+    const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .uplaod(imageName, newCabin.image);
+
+    // 3. Delete the cabin IF there was an error uploading image
+    if(storageError) {
+      await supabase
+      .from("cabins")
+      .delete()
+      .eq("id",data.id);
+      console.error(storageError);
+      throw new Error("Cabins image could not be uploaded and the cabin was not created");
+    }
+
+    return data;
+}
+
 export async function deleteCabins(id){
     const {data, error} = await supabase
       .from("cabins")
