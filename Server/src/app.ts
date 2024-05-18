6dfb84de010759f7a2de726f65158de8806fd18a
RadonZ <supachai27102000@gmail.com>
2024-05-19 00:05:30 +0700

    create server

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,1 +1,9 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
+
+
+
+// 1) MIDDLEWARE
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+    }
