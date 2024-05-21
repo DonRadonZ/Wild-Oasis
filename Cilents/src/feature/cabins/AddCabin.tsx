e0bcd4677071ad95a7a6a1cda8d71c31da211274
RadonZ <supachai27102000@gmail.com>
2024-05-21 23:41:00 +0700

    update: create modal

--- Cilents/src/feature/cabins/AddCabin.tsx
+++ Cilents/src/feature/cabins/AddCabin.tsx
@@ -3,19 +3,37 @@ import Button from '../../ui/Button/Button';
 import CreateCabinForm from './CreateCabinForm';
 import Modal from '../../ui/Modal/Modal';
 
-export default function AddCabin() {
-    const [isOpenModal, setIsOpenModal] = useState(false);
 
+export default function AddCabin() {
   return (
     <div>
-        <Button onClick={() => setIsOpenModal((show) => !show)}>
-            Add new cabin
-        </Button>
-        {isOpenModal && 
-        <Modal onClose={() => setIsOpenModal(false)}>
-            <CreateCabinForm onClose={() => setIsOpenModal(false)}/>
-        </Modal>
-        }
+    <Modal>
+      <Modal.Open opens='cabin-form'>
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
     </div>
-  )
+  );
 }
+
+
+
+// export default function AddCabin() {
+//     const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//         <Button onClick={() => setIsOpenModal((show) => !show)}>
+//             Add new cabin
+//         </Button>
+//         {isOpenModal && 
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//             <CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/>
+//         </Modal>
+//         }
+//     </div>
+//   )
+// }
