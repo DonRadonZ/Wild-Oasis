29b51922c06e78f1e3e325291504ff1353f23664
RadonZ <supachai27102000@gmail.com>
2024-05-22 23:59:36 +0700

    Add: Booking and Checkin page

--- Cilents/src/App.tsx
+++ Cilents/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 import Dashboard from "./page/Dashboard";
 import Bookings from "./page/Bookings";
@@ -13,7 +14,9 @@ import PageNotFound from "./page/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout/AppLayout";
 import DarkModeProvider from "./context/DarkModeContext";
-import { Toaster } from "react-hot-toast";
+
+import Booking from "./page/Booking";
+import Checkin from "./page/Checkin";
 
 
 
@@ -38,6 +41,8 @@ export default function App() {
             <Route index element={<Navigate replace to="dashboard"/>} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings/>} />
+            <Route path="bookings/:bookingId" element={<Booking/>} />
+            <Route path="checkin/:bookingId" element={<Checkin/>} />
             <Route path="cabins" element={<Cabins/>} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
