baa53bbd9e5bdfa081b5b098a3208209a264a246
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-05-23 23:26:44 +0700

    add: signup

--- Cilents/src/page/Users.tsx
+++ Cilents/src/page/Users.tsx
@@ -1,8 +1,14 @@
+import SignupForm from "../feature/authentication/SignupForm";
 import Heading from "../ui/Heading/Heading";
 
 
 function NewUsers() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <>
+    <Heading as="h1">Create a new user</Heading>
+      <SignupForm />
+    </>
+  );
 }
 
 export default NewUsers;
