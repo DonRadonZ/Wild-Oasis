a1102b05ee33431fb6c9e29bc2db2aa5fe647d58
RadonZ <supachai27102000@gmail.com>
2024-06-10 22:25:00 +0700

    refactor: remove unused part

--- Cilents/src/feature/cabins/CabinTable.tsx
+++ Cilents/src/feature/cabins/CabinTable.tsx
@@ -1,31 +1,14 @@
-import styled from "styled-components";
-
 import { useSearchParams } from "react-router-dom";
 
 import Spinner from "../../ui/Spinner/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabin";
-import Empty from "../../ui/Empty/Empty";
+// import Empty from "../../ui/Empty/Empty";
 import Table from "../../ui/Table/Table";
-import { cabins } from '../../data/data-cabins';
+
 import Menus from "../../ui/Menus/Menus";
 
 
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
 
 
 export default function CabinTable() {
@@ -33,22 +16,22 @@ export default function CabinTable() {
   const [searchParams] = useSearchParams();
 
   if(isLoading) return <Spinner/>;
-  if(!cabins.length) return <Empty resourceName="bookings"/>
+  // if(!cabins.length) return <Empty resourceName="bookings"/>
 
   // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
   if(filterValue === "all") filteredCabins = cabins;
-  if (filterValue === "no-discount") filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "no-discount") filteredCabins = cabins?.filter((cabin) => cabin.discount === 0);
 
-  if (filterValue === "with-discount") filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+  if (filterValue === "with-discount") filteredCabins = cabins?.filter((cabin) => cabin.discount > 0);
 
   // 2) SORT
   const sortBy = searchParams.get('sortBy') || "startDate-asc"
   const [field, direction] = sortBy.split("-");
   const modifier = direction === "asc" ? 1 : -1
-  const sortedCabins =  filteredCabins.sort((a,b) => (a[ field] - b[field]) * modifier);
+  const sortedCabins =  filteredCabins.sort((a,b) => (a[field] - b[field]) * modifier);
 
 
   return (
