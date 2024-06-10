3f9027b9207086ab5ab19a6901616d2355fc9831
RadonZ <supachai27102000@gmail.com>
2024-06-10 22:25:31 +0700

    add data

--- Cilents/src/services/apiBooking.ts
+++ Cilents/src/services/apiBooking.ts
@@ -122,7 +122,7 @@ export async function updateBooking(id: any, obj: any) {
 
 export async function deleteBooking(id: any) {
   // REMEMBER RLS POLICIES
-  // const { data, error } = await supabase.from("bookings").delete().eq("id", id);
+  const { data, error } = await supabase.from("bookings").delete().eq("id", id);
 
   if (error) {
     console.error(error);
