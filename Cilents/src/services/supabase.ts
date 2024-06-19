f092ed4f5dcdca08b5c1994baefaf4bf0bf3e408
Supachai Sinkaseam <supachai27102000@gmail.com>
2024-06-19 19:40:36 +0700

    add: supabase and uploader

---
+++ Cilents/src/services/supabase.ts
@@ -0,0 +1,11 @@
+
+import { createClient} from '@supabase/supabase-js'
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+
+
+export const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const imageBucketName = "wila-oasis-images";
+
+export default supabase;
