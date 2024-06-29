1b3f1b9baf4f004079bce2e3aca159adfa6f4e9c
RadonZ <supachai27102000@gmail.com>
2024-06-29 21:10:45 +0700

    clean unused

--- Clients/src/feature/authentication/useSignUp.ts
+++ Clients/src/feature/authentication/useSignUp.ts
@@ -6,7 +6,7 @@ export function useSignUp() {
 
     const {mutate: signup, isLoading } = useMutation({
         mutationFn: signupApi,
-        onSuccess: (user) => {
+        onSuccess: () => {
             toast.success("Account successfully created! Please verify the new account from the user\'s email address.");
         }
     }) 
