7c4cd9f69fbbaa9d5adca5c48dd9b3c6e260c5bc
RadonZ <supachai27102000@gmail.com>
2024-07-23 22:37:08 +0700

    update app

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,9 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
+import express, { Express } from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
+const app: Express = express();
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
-// 1) MIDDLEWARE
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-    }
+app.use(express.json());
+
+const mongoURI: string | undefined = process.env.MONGOURI?.replace('<PASSWORD>', process.env.DATABASE_PASSWORD || '');
+
+if (!mongoURI) {
+    throw new Error("MongoURI is not defined in the environment variables");
+}
+
+mongoose
+    .connect(mongoURI)
+    .then(() => console.log("CONNECTED TO MONGODB!"))
+    .catch((err: Error) => console.error("Failed to Connect to MongoDB: ", err))
+
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`)
+})
